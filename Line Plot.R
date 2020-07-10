2c817f19ea1ea6d57a0b4629469ed93b3dc7e4a8
geoffhan <geoffrey.hansen12@gmail.com>
2020-07-10 15:04:54 -0400

    Update Line Plot.R

--- Line Plot.R
+++ Line Plot.R
@@ -2,12 +2,13 @@ library(tidyverse)
 
 library(ggplot2)
 
-data <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM")
+data <- filter(
+  read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM"),
+  countriesAndTerritories =="United_States_of_America"|countriesAndTerritories =="United_Kingdom"|countriesAndTerritories == "South_Korea"|countriesAndTerritories == "China")
 
-df <- data.frame('dateRep','cases')
-
-ggplot(data, aes(x=dateRep, y=cases, group=1)) +
-  geom_line(color="red")+
-  geom_point()
-
-plt.show()
+ggplot(data, aes(x=dateRep, y=cases, group=countriesAndTerritories, color=countriesAndTerritories)) +
+    geom_smooth(aes(color=countriesAndTerritories))+
+    labs(title = "Pandemic")+
+    ylab("Cases")+
+    xlab("Date")+
+    scale_y_continuous(trans = 'log10')
